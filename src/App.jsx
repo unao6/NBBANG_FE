64a067b44546a6bdf0c3e8071dead26dccbff5b6
lchyeong <lchyeong@gmail.com>
2024-07-23 10:36:01 +0900

    chore: mui,tailwind 공통 폰트 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 function App() {
   return 
---
+++ src/Router.jsx
@@ -0,0 +1,10 @@
+import { Route, Routes } from 'react-router-dom';
+
+const Router = () => {
+  return (
+    <Routes>
+    </Routes>
+  );
+};
+
+export default Router;
