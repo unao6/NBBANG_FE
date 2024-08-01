624f5cc4e60961f4726d93954d6844912cdb937c
[Cloud3] 김현필 <gusvlf1028@naver.com>
2024-08-01 14:12:11 +0900

    feat: 카드 등록 FE 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,14 +9,14 @@ import prettierConfig from "eslint-plugin-prettier";
 export default defineConfig([
   {
     languageOptions: {
-      parser: '@babel/eslint-parser',
+      parser: "@babel/eslint-parser",
       parserOptions: {
         requireConfigFile: false,
         ecmaVersion: 2021,
-        sourceType: 'module',
-        babelOptions: {
-          presets: ['@babel/preset-react'],
-        },
+        sourceType: "module",
+        // babelOptions: {
+        //   presets: ["@babel/preset-react"],
+        // },
       },
       globals: globals.browser,
     },
