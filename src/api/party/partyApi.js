1e0d5255679d7f551dcba1a3b611e4f37e9bc7dd
opixxx <opix0306@naver.com>
2024-08-02 21:41:12 +0900

    feat: Party API 추가

--- src/api/party/partyApi.js
+++ src/api/party/partyApi.js
@@ -3,3 +3,11 @@ import axiosInterceptors from "../axiosInterceptors.js";
 export const createParty = (partyData) => {
   return axiosInterceptors.post(`/api/party`, partyData);
 };
+
+export const subscribeOtt = () => {
+  return axiosInterceptors.get('/api/subscribed-otts')
+};
+
+export const partyMatching = (partyMatching) => {
+  return axiosInterceptors.post('/api/matching', partyMatching)
+};
