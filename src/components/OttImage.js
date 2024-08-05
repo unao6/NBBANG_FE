4a09cd9a2b7965ed54842f174919edcbe2b7f9a3
opixxx <opix0306@naver.com>
2024-08-05 11:02:34 +0900

    refactor: toLowerCase 를 이용해서 switch case 문 변경

--- src/components/OttImage.js
+++ src/components/OttImage.js
@@ -1,10 +1,10 @@
 export const getOttImage = (name) => {
-  switch (name) {
-    case '디즈니플러스':
+  switch (name.toLowerCase()) {
+    case 'disney+':
       return "/assets/imgs/disney.png";
-    case 'ChatGPT':
+    case 'chatgpt':
       return "/assets/imgs/ChatGPT.png";
-    case '티빙':
+    case 'tving':
       return "/assets/imgs/tving.png";
     default:
       return "/assets/imgs/default.png"; // 기본 이미지 경로
