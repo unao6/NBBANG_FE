4aca0c03aec4fcf5b282b67bbadfe8b5d3fe9ed2
lchyeong <lchyeong@gmail.com>
2024-08-10 13:28:53 +0900

    feat: account MyPage UI 수정 및 은행리스트,svg 파일 추가

---
+++ src/components/BankImage.js
@@ -0,0 +1,65 @@
+export const getBankImage = (name) => {
+    switch (name) {
+      case "경남은행":
+        return "/assets/imgs/bank/bankName=경남.svg";
+      case "광주은행":
+        return "/assets/imgs/bank/bankName=광주.svg";
+      case "대구은행":
+        return "/assets/imgs/bank/bankName=대구.svg";
+      case "부산은행":
+        return "/assets/imgs/bank/bankName=부산.svg";
+      case "산림조합":
+        return "/assets/imgs/bank/bankName=산림조합.svg";
+      case "새마을금고":
+        return "/assets/imgs/bank/bankName=새마을.svg";
+      case "수협은행":
+        return "/assets/imgs/bank/bankName=수협.svg";
+      case "신한은행":
+        return "/assets/imgs/bank/bankName=신한.svg";
+      case "신협은행":
+        return "/assets/imgs/bank/bankName=신협.svg";
+      case "씨티은행":
+        return "/assets/imgs/bank/bankName=씨티.svg";
+      case "우리은행":
+        return "/assets/imgs/bank/bankName=우리.svg";
+      case "우체국":
+        return "/assets/imgs/bank/bankName=우체국.svg";
+      case "저축은행":
+        return "/assets/imgs/bank/bankName=저축은행.svg";
+      case "전북은행":
+        return "/assets/imgs/bank/bankName=전북.svg";
+      case "제주은행":
+        return "/assets/imgs/bank/bankName=제주.svg";
+      case "카카오뱅크":
+        return "/assets/imgs/bank/bankName=카카오뱅크.svg";
+      case "케이뱅크":
+        return "/assets/imgs/bank/bankName=케이뱅크.svg";
+      case "토스뱅크":
+        return "/assets/imgs/bank/bankName=토스뱅크.svg";
+      case "하나은행":
+        return "/assets/imgs/bank/bankName=하나.svg";
+      case "한국투자증권":
+        return "/assets/imgs/bank/bankName=한국투자증권.svg";
+      case "IBK기업은행":
+        return "/assets/imgs/bank/bankName=IBK기업.svg";
+      case "KB국민은행":
+        return "/assets/imgs/bank/bankName=KB국민.svg";
+      case "KB증권":
+        return "/assets/imgs/bank/bankName=KB증권.svg";
+      case "KDB산업은행":
+        return "/assets/imgs/bank/bankName=KDB산업.svg";
+      case "KEB외환은행":
+        return "/assets/imgs/bank/bankName=KEB외환.svg";
+      case "NH농협은행":
+        return "/assets/imgs/bank/bankName=NH농협.svg";
+      case "NH투자증권":
+        return "/assets/imgs/bank/bankName=NH투자증권.svg";
+      case "SBI저축은행":
+        return "/assets/imgs/bank/bankName=SBI저축.svg";
+      case "SC제일은행":
+        return "/assets/imgs/bank/bankName=SC제일.svg";
+      default:
+        return "/assets/imgs/default.png";
+    }
+  };
+  
--- src/pages/payment/AccountMyPage.jsx
+++ src/pages/payment/AccountMyPage.jsx
@@ -1,167 +1,124 @@
+import { Box, Button, Card, CardContent, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import {
-    Alert,
-    Box,
-    Button,
-    Card,
-    CardContent,
-    Snackbar,
-    Typography,
-  } from "@mui/material";
-  import React, { useEffect, useState } from "react";
-  import { getAccountInfo, deleteAccountInfo } from "../../api/payment/accountApi";
-  
-  import AddIcon from "@mui/icons-material/Add";
-  import Container from "../../components/Container";
-  import DeleteIcon from "@mui/icons-material/Delete";
-  import EditIcon from "@mui/icons-material/Edit";
-import { useNavigate } from "react-router-dom";
-  
-  const AccountMyPage = () => {
-    const userId = 1; // 사용자의 ID를 설정합니다. 실제로는 로그인된 사용자의 ID를 가져와야 합니다.
-    const [accountInfo, setAccountInfo] = useState(null);
-    const [modalOpen, setModalOpen] = useState(false); // 모달 상태 관리
-    const [snackbarOpen, setSnackbarOpen] = useState(false); // Snackbar 상태 관리
-    const navigate = useNavigate();
-  
-    useEffect(() => {
-      const fetchAccountInfo = async () => {
-        try {
-          const data = await getAccountInfo(userId);
-          console.log("Fetched account info:", data);
-          setAccountInfo(data);
-        } catch (error) {
-          console.error("카드 정보를 불러오는 중 오류 발생:", error);
-        }
-      };
-  
-      fetchAccountInfo();
-    }, [userId]);
-  
-    const handleRegister = () => {
-      navigate("/mypage/account/register")
-    };
-  
-    const handleCloseSnackbar = () => {
-      // Snackbar를 닫는 함수
-      setSnackbarOpen(false);
-    };
-  
-    const handleDelete = async () => {
-      try {
-        await deleteAccountInfo(userId);
-        setAccountInfo(null);
-        setSnackbarOpen(true); // 삭제 성공 시 Snackbar를 엽니다.
-      } catch (error) {
-        console.error("카드 삭제 중 오류 발생:", error);
-      }
-    };
-  
-    const handleChange = () => {
-      navigate("/mypage/account/register")
-    };
-  
-    return (
-      <Container>
-        <Box className="p-4 flex flex-col items-center">
-          <Box className="w-full bg-white rounded-lg shadow p-4 mt-4">
-            <Typography variant="h6" component="div" gutterBottom>
-              현재 정산계좌
-            </Typography>
-            {accountInfo ? (
-              <>
-                <Card
-                  className="my-4 mx-auto"
-                  sx={{
-                    width: "300px",
-                    height: "180px",
-                    backgroundColor: "#8EF740",
-                    position: "relative",
-                  }}
-                >
-                  <CardContent>
-                    <Typography variant="h5" component="div">
-                      {accountInfo.bankName}
-                    </Typography>
-                    <Typography
-                      color="textSecondary"
-                      sx={{ position: "absolute", bottom: 16, right: 16 }}
-                    >
-                      {accountInfo.accountNumber}
-                    </Typography>
-                  </CardContent>
-                </Card>
-                <Box
-                  sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    gap: 2,
-                    mt: 4,
-                  }}
-                >
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    startIcon={<EditIcon />}
-                    onClick={handleChange}
-                  >
-                    정산 계좌 변경
-                  </Button>
-                  <Button
-                    variant="contained"
-                    color="secondary"
-                    startIcon={<DeleteIcon />}
-                    onClick={handleDelete}
-                  >
-                    정산 계좌 제거
-                  </Button>
-                </Box>
-              </>
-            ) : (
-              <>
-                <Card
-                  className="my-4 mx-auto flex justify-center items-center"
-                  sx={{ width: "300px", height: "180px" }}
-                >
-                  <CardContent>
-                    <Typography variant="h5" component="div">
-                      등록된 정산수단이 없어요
-                    </Typography>
-                    <Typography color="textSecondary" className="mb-4">
-                      엔빵 서비스 이용을 위해 정산 수단을 등록해 주세요.
-                    </Typography>
-                  </CardContent>
-                </Card>
-                <Box sx={{ display: "flex", justifyContent: "center", mt: 4 }}>
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    startIcon={<AddIcon />}
-                    onClick={handleRegister}
-                    sx={{ width: "50%" }}
-                  >
-                    정산 수단 등록하기
-                  </Button>
-                </Box>
-              </>
-            )}
-          </Box>
-          <Snackbar
-            open={snackbarOpen}
-            autoHideDuration={6000}
-            onClose={handleCloseSnackbar}
-          >
-            <Alert
-              onClose={handleCloseSnackbar}
-              severity="success"
-              sx={{ width: "100%" }}
-            >
-              정산 계좌가 성공적으로 삭제되었습니다!
-            </Alert>
-          </Snackbar>
-        </Box>
-      </Container>
-    );
+  deleteAccountInfo,
+  getAccountInfo,
+} from "../../api/payment/accountApi";
+
+import AccountRegisterModal from "./fragments/AccountRegisterModal";
+import { getBankImage } from "../../components/BankImage";
+
+const AccountMyPage = () => {
+  const userId = 1;
+  const [accountInfo, setAccountInfo] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const fetchAccountInfo = async () => {
+    try {
+      const data = await getAccountInfo(userId);
+      setAccountInfo(data);
+    } catch (error) {
+      console.error("Error fetching account info:", error);
+    }
   };
-  
-  export default AccountMyPage;
-  
+
+  useEffect(() => {
+    fetchAccountInfo();
+  }, [userId]);
+
+  const handleRegisterOrChange = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
+  const handleDelete = async () => {
+    try {
+      await deleteAccountInfo(userId);
+      setAccountInfo(null);
+    } catch (error) {
+      console.error("Error deleting account info:", error);
+    }
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: "24px",
+        bgcolor: "#F7FAFC",
+        borderRadius: "12px",
+        boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
+        maxWidth: "500px",
+        margin: "auto",
+        marginTop: "32px",
+      }}
+    >
+      <Typography
+        variant="h6"
+        sx={{ marginBottom: "16px", fontWeight: "bold" }}
+      >
+        다음 계좌로 매달 안전히 정산해드릴게요
+      </Typography>
+      <Card
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          padding: "16px",
+          bgcolor: "#FFFFFF",
+          borderRadius: "12px",
+          boxShadow: "0 2px 8px rgba(0, 0, 0, 0.1)",
+          width: "100%",
+          marginBottom: "24px",
+        }}
+      >
+        {accountInfo ? (
+          <>
+            <img
+              src={getBankImage(accountInfo.bankName)}
+              alt={accountInfo.bankName}
+              style={{ width: "48px", height: "48px", marginRight: "16px" }}
+            />
+            <CardContent sx={{ flex: 1 }}>
+              <Typography variant="h6" fontWeight="bold">
+                {accountInfo.bankName}
+              </Typography>
+              <Typography variant="body1" sx={{ color: "gray" }}>
+                {accountInfo.accountNumber}
+              </Typography>
+            </CardContent>
+            <Button
+              onClick={handleRegisterOrChange}
+              sx={{ color: "#007BFF", fontWeight: "bold" }}
+            >
+              변경
+            </Button>
+          </>
+        ) : (
+          <CardContent sx={{ flex: 1, textAlign: "center" }}>
+            <Typography variant="body1" sx={{ color: "gray" }}>
+              등록된 계좌가 없습니다.
+            </Typography>
+          </CardContent>
+        )}
+      </Card>
+      <Button
+        onClick={accountInfo ? handleDelete : handleRegisterOrChange}
+        sx={{ color: "primary.main", fontWeight: "bold", marginBottom: "8px" }}
+      >
+        {accountInfo ? "등록된 계좌 삭제하기" : "계좌 등록하기"}
+      </Button>
+      <AccountRegisterModal
+        open={modalOpen}
+        onClose={handleCloseModal}
+        onSubmitSuccess={fetchAccountInfo}
+      />
+    </Box>
+  );
+};
+
+export default AccountMyPage;
