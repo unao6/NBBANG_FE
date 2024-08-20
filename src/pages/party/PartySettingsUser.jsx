facfa6d7c530416ed351de07cd40641e25611191
lchyeong <lchyeong@gmail.com>
2024-08-20 09:58:55 +0900

    fix: 계좌변경 오류 수정

--- src/pages/party/PartySettingsUser.jsx
+++ src/pages/party/PartySettingsUser.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+
 import { fetchUserInfo } from "../../api/user/userApi";
 
 const PartySettingsUser = () => {
